c008446e0304058ec89ed2c9825b73c5e91cbafb
killian-lecorf <Klecorf@normandiewebschool.fr>
2025-03-31 11:28:42 +0200

    fix: lint

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col items-center p-4">
-      <h1 className="text-2xl mb-4">Bienvenue sur la page d'accueil</h1>
+      <h1 className="text-2xl mb-4">Bienvenue sur la page d&apos;accueil</h1>
       <Image
         src="/react.jpg"
         alt="Une belle image"
